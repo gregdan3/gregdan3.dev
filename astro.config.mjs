65dcedb67d6b1821845c5d7414e05c9cb4fa446c
Gregory Danielson <gregory.danielson3@gmail.com>
2024-09-13 13:18:57 -0500

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
-import remarkGfm from 'remark-gfm'
-import remarkSmartypants from 'remark-smartypants'
-import rehypeExternalLinks from 'rehype-external-links'
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+import remarkGfm from "remark-gfm";
+import remarkSmartypants from "remark-smartypants";
+import rehypeExternalLinks from "rehype-external-links";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
+  site: "https://gregdan3.dev",
+  base: "/",
+  publicDir: "./static",
+  integrations: [mdx(), svelte(), sitemap(), icon()],
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: "nord",
     },
     remarkPlugins: [remarkGfm, remarkSmartypants],
     rehypePlugins: [
       [
         rehypeExternalLinks,
         {
-          target: '_blank',
+          target: "_blank",
         },
       ],
     ],
   },
-})
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
+});
