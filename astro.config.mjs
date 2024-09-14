16ac7d3150f5322fcb43db6fa960f07d5617a132
Gregory Danielson <gregory.danielson3@gmail.com>
2024-09-14 17:42:41 -0500

    brrrrr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,43 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import rehypeExternalLinks from "rehype-external-links";
 import remarkGfm from "remark-gfm";
 import remarkSmartypants from "remark-smartypants";
-import rehypeExternalLinks from "rehype-external-links";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
-
 import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://gregdan3.dev",
   base: "/",
-  publicDir: "./static",
-  integrations: [mdx(), svelte(), sitemap(), icon(), robotsTxt()],
+  publicDir: "./public",
+  integrations: [mdx(), sitemap(), icon(), robotsTxt()],
   markdown: {
     shikiConfig: {
-      theme: "nord"
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      wrap: true,
     },
     remarkPlugins: [remarkGfm, remarkSmartypants],
-    rehypePlugins: [[rehypeExternalLinks, {
-      target: "_blank"
-    }]]
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+        },
+      ],
+    ],
   },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin"
-    }
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
   },
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
 });
---
+++ src/content/about/about-me.md
@@ -0,0 +1,10 @@
+I'm Gregory (@gregdan3). I do devops, database, and backend. Frontend was
+suspiciously missing from this list, so this site is now a permanent work in
+progress as I learn ~~Svelte~~ Astro. I have no idea how to breathe.
+
+Here's a second line of dialogue in case I think of something important to say.
+Nope, not coming to me.
+
+- one
+- two
+- three
