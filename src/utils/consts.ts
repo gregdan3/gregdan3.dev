1ec100be406204780e81f709f55ea20ca35c084d
Gregory Danielson <gregory.danielson3@gmail.com>
2024-10-02 14:39:15 -0500

    feat: ethical tracking
    goatcounter is based

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -2,3 +2,5 @@ export const menuToggleId = "menutoggle"; // sincerely.
 
 export const discordUserID = "497549183847497739";
 export const LANYARD_URL = "https://api.lanyard.rest/v1";
+
+export const goatcounterEndpoint = "https://gregdan3.goatcounter.com/count";
