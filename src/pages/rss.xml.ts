c4be172c0dbbd4a76e680c58b30269b9dd5497e1
Gregory Danielson <gregory.danielson3@gmail.com>
2024-10-02 14:40:52 -0500

    fix: add projects to rss; make typing of rss file more sane

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,17 +2,29 @@ import { marked } from "marked";
 import rss from "@astrojs/rss";
 import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
 
 export async function GET(context: APIContext) {
-  let posts = await getCollection("blog", (post) => post.data.published);
-  posts = posts.sort(
-    (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
+  let posts: (CollectionEntry<"blog"> | CollectionEntry<"projects">)[] = [];
+
+  const blog = await getCollection(
+    "blog",
+    (post) => (post.data.published && post.body) || import.meta.env.DEV,
+  );
+  const projects = await getCollection(
+    "projects",
+    (post) => (post.data.published && post.body) || import.meta.env.DEV,
   );
 
-  posts = posts.slice(0, 8);
+  posts.push(...blog);
+  posts.push(...projects);
+
+  posts = posts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
+  posts = posts.slice(0, 10);
 
   posts = await Promise.all(
     posts.map(async (post) => {
+      // rendering the description is dumb, this isn't html
       // post.data.description = post.data.description
       //   ? await marked.parse(post.data.description)
       //   : undefined;
@@ -32,7 +44,7 @@ export async function GET(context: APIContext) {
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
-      link: `/blog/${post.slug}`,
+      link: `/${post.collection}/${post.slug}`,
       // content: post.body,
     })),
   });
