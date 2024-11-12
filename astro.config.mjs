09cf0157eaef71b51909d65090ef57ae59e6d3a2
Gregory Danielson <gregory.danielson3@gmail.com>
2024-11-12 12:22:51 -0600

    chore: update deps
    mostly cleaning, nothing material implemented

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 import compress from "astro-compress";
 import icon from "astro-icon";
+import pagefind from "astro-pagefind";
+import purgecss from "astro-purgecss";
 import robotsTxt from "astro-robots-txt";
 
 import rehypeExternalLinks from "rehype-external-links";
 import { remarkMark } from "remark-mark-highlight";
 import remarktoc from "remark-toc";
 
-import pagefind from "astro-pagefind";
+import playformInline from "@playform/inline";
 
 export default defineConfig({
   site: "https://gregdan3.dev",
@@ -20,9 +23,11 @@ export default defineConfig({
   integrations: [
     mdx(),
     icon({ iconDir: "src/icons" }),
+    pagefind(),
+    purgecss(),
+    playformInline({}),
     sitemap(),
     robotsTxt(),
-    pagefind(),
     compress(),
   ],
   markdown: {
